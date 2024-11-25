4c2fc63a50c154d9982d1b781d817f52d1e9986a
Hamid649 <gamid.mahmudov.gm@gmail.com>
2024-11-25 13:21:50 +0000

    Add

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
-int main()
-
+#include<stdio.h>
+/**
+ *  main - This is the entry point of the code
+ *
+ *  Return: 0 Successful
+ */
+int main(void)
 {
-
-puts ("\"Programming is like building a multilingual puzzle");
-
-return 0;
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
- 
