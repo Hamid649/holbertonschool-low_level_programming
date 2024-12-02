7f0935b0d90c16f75441399e102971deb402622e
Hamid649 <gamid.mahmudov.gm@gmail.com>
2024-12-02 15:37:34 +0400

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: 0 Successful
+ */
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+    putchar('\n');
+
+    return (0);
+}
+
